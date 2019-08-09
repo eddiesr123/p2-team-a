a66be6cb5212b87a2ae8c68ddcf624dfd6798f21
Sam Yusupov <sergyusup@gmail.com>
2019-08-09 09:06:05 -0400

    Added signin, signup pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,11 @@ import React from 'react';
 import './App.css';
 import AppRouter from './components/Router';
 import './include/bootstrap';
-import { Provider } from 'react-redux'
-import { store } from './Store'
 
 const App: React.FC = () => {
   return (
     <div id='router-container'>
-      <Provider store={store} >
         <AppRouter />
-      </Provider>
     </div>
   );
 }
