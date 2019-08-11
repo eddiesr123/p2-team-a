95ea3ae301b40b6e037bdbee042b2a58db8ccfa5
chaparrovision <david.chaparro@ymail.com>
2019-08-11 15:17:08 -0400

    changed grid to 6 each

--- src/components/Browse.tsx
+++ src/components/Browse.tsx
@@ -24,8 +24,8 @@ function Browse() {
                 </tbody>
             </table>*/}
             <div className="row">
-                <div className="col-sm-9 col-md-9" id="left-col"><BrowseTabComponent></BrowseTabComponent></div>
-                <div className="col-sm-3 col-md-3" id="right-col"><Display/></div>
+                <div className="col-sm-6 col-md-6" id="left-col"><BrowseTabComponent></BrowseTabComponent></div>
+                <div className="col-sm-6 col-md-6" id="right-col"><Display/></div>
 
             </div>
         </div>  
