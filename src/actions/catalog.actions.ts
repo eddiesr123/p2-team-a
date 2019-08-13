6e39ec1f892624e4b1f89a37fb66bc9ccf9f4992
Jessica Waltz <jessabelleanna@gmail.com>
2019-08-13 18:14:01 -0400

    set up actions and reducers, now can display actual items to catalog

---
+++ src/actions/catalog.actions.ts
@@ -0,0 +1,25 @@
+import { catalogConstants } from '../constants/catalog.constants';
+
+export const catalogActions = {
+    updateSuit,
+    updateMask,
+    updateGloves
+};
+
+function add(message: any) {
+    return { type: catalogConstants.ADD, message };
+}
+
+function view(message: any) {
+    return { type: catalogConstants.VIEW, message };
+}
+
+function updateSuit(image: any) {
+    return { type: catalogConstants.UPDATE_SUIT,image };
+}
+function updateMask(image: any) {
+    return { type: catalogConstants.UPDATE_MASK,image };
+}
+function updateGloves(image: any) {
+    return { type: catalogConstants.UPDATE_GLOVES,image };
+}
