97973ae7ee8150dddf2eb0242acc65a3b9d11914
chaparrovision <david.chaparro@ymail.com>
2019-08-14 17:31:53 -0400

    added initial Redux connections

--- src/components/Display/Display.tsx
+++ src/components/Display/Display.tsx
@@ -2,8 +2,10 @@ import React from "react";
 import { Bodysuit } from "./Bodysuit";
 import { Mask } from "./Mask";
 import { Gloves } from "./Gloves";
-import { IDisplayState } from "../../reducers";
+import { IDisplayState, IState } from "../../reducers";
 import { any } from "prop-types";
+import { connect } from "react-redux";
+
 
 export interface IDisplayProps {
   display: IDisplayState;
@@ -16,9 +18,24 @@ export class Display extends React.Component<any, IDisplayProps> {
         <Bodysuit />
         <Mask />
         <Gloves />
+        <span>{this.props.total}</span>
       </div>
     );
   }
 }
 
-export default Display;
+const mapStateToProps = (state: IState) => {
+  return {
+    display: state.display,
+    suit: state.catalog.suit,
+    mask: state.catalog.mask,
+    gloves: state.catalog.gloves,
+    total: state.cart.totalItems
+
+  }
+}
+
+const mapDispatchToProps = {
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Display);
