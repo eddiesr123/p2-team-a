e3ee4afd0a252aa7c5c70c1476a8d9fa970605e3
Jessica Waltz <jessabelleanna@gmail.com>
2019-08-15 18:30:33 -0400

    more dynamic

--- src/components/CatalogCard.tsx
+++ src/components/CatalogCard.tsx
@@ -3,29 +3,28 @@ import '../css/fontawesome-free-5.10.1-web/fontawesome-free-5.10.1-web/css/all.c
 
 export default class ItemCardComponent extends React.Component<any, any> {
     render() {
-        return ( 
-        <div className="col-3 catalog-card">
-        <div className="card card-item" >
-            <img className="card-img-top set-img-card" id="cards" src={this.props.imgObj} alt="Card image cap"/>
-            <div className="card-body">
-                <h5 className="card-title">Item Name</h5>
-                    <div className="container">
-                        <div className="row">
-                            <div className="col-4">
-                                price
-                            </div>
-                            <div className="col-4">
-                                <a href="#" className="btn btn-outline-secondary"><i className="fas fa-plus"></i></a>
-                            </div>
-                            <div className="col-4">
-                                <a href="#" className="btn btn-outline-secondary" onClick={()=>(this.props.updateDisplay("../" + this.props.pathToImg,this.props))}><i className="far fa-eye"></i></a>
+        return (
+            <div className="col-sm-12 col-md-3 col-lg-3 catalog-card">
+                <div className="card card-item" >
+                    <img className="card-img-top set-img-card" id="cards" src={this.props.imgObj} alt="Card image cap" />
+                    <div className="card-body">
+                        <h5 className="card-title">{this.props.name}</h5>
+                        <div className="container">
+                            <div className="row">
+                                <div className="col-sm-4 col-md-12 col-lg-4">
+                                    <a href="#" className="btn btn-outline-secondary diabled">$ {this.props.price}</a>
+                                </div>
+                                <div className="col-sm-4 col-md-12 col-lg-4">
+                                    <a href="#" className="btn btn-outline-secondary"><i className="fas fa-plus"></i></a>
+                                </div>
+                                <div className="col-sm-4 col-md-12 col-lg-4">
+                                    <a href="#" className="btn btn-outline-secondary" onClick={() => (this.props.updateDisplay("../" + this.props.pathToImg, this.props))}><i className="far fa-eye"></i></a>
+                                </div>
                             </div>
                         </div>
                     </div>
-                   
+                </div>
             </div>
-      </div>
-      </div>
         )
     }
 }
