ffcbe4f33e90be187d46d9a95ac3273ce9632e77
Sam Yusupov <sergyusup@gmail.com>
2019-08-16 11:37:21 -0400

    Added private routes and 404 page

--- src/components/Logout.tsx
+++ src/components/Logout.tsx
@@ -1,7 +1,4 @@
 import React from 'react';
-import { Redirect } from "react-router";
-import { IState, ISignInState } from '../reducers';
-import { userActions } from '../actions/user.actions';
 import { IState } from '../reducers';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
 
 export class notFoundPage extends React.Component<any, any> {
-    render(){
-    return <h2>404 Oops...</h2>;
+    render() {
+    return (
+      <div id="notfound">
+      <div className="notfound">
+        <div className="notfound-404">
+          <h1>4<span>0</span>4</h1>
+        </div>
+        <p>The page you are looking for might have been removed had its name changed or is temporarily unavailable.</p>
+        <a href="/">home page</a>
+      </div>
+    </div>
+     );
   }
 }
 
