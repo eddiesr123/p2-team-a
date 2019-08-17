d25f4fab72cc7222fbf9176b0b09f2716a99e39c
chaparrovision <david.chaparro@ymail.com>
2019-08-17 09:27:32 -0400

    added empty pngs, now model does not have mask and gloves on load

--- src/components/Display/Mask.tsx
+++ src/components/Display/Mask.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 const imgMap: any = {'../../images/alphas/mask-black-female.png': require('../../images/alphas/mask-black-female.png'),
                     '../../images/alphas/mask-blue-female.png': require('../../images/alphas/mask-blue-female.png'),
                     '../../images/alphas/mask-green-female.png': require('../../images/alphas/mask-green-female.png'),
+                    '../../images/defaults/empty-female-mask.png': require('../../images/defaults/empty-female-mask.png'),
                     '../../images/alphas/mask-red-female.png': require('../../images/alphas/mask-red-female.png')}
 
 export class Mask extends React.Component<any, any> {
