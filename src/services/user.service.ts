bc5089ce363e5fe3635f4ac4041ff1b649abd574
Dylan Adams <dadams10642@outlook.com>
2019-08-17 14:01:10 -0400

    response goes to backend and comes back as the same

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,5 +1,7 @@
-const api_url = 'http://localhost:8080/users';
+import Axios from 'axios';
 
+
+const api_url = 'http://localhost:8080/users';
 export const userService = {
     signup,
     signin,
@@ -15,7 +17,7 @@ async function signup(user: any) {
         body: JSON.stringify({ username, firstName, lastName, email, password })
     };
     return await fetch(`${api_url}/signup`, requestOptions)
-        .then((res) => {console.log(res); return res;})
+        .then((res) => { console.log(res); return res; })
         .then(handleResponse)
         .then(response => response.json())
         .then(user => {
@@ -41,18 +43,19 @@ async function signin(user: any) {
 async function changeInfo(user: any) {
     const { username, firstName, lastName, email, password } = user;
     const creditCard = { number: user.creditCard };
-    const requestOptions = {
-        method: 'PUT',
-        headers: { 'Authorization' : user.token, 'Content-Type': 'application/json' },
-        body: JSON.stringify({ username, firstName, lastName, email, password, creditCard })
-    }; console.log(requestOptions);
-
-    return await fetch(`${api_url}/userinfo`, requestOptions)
+    const body = { username, firstName, lastName, email, password, creditCard }
+    let config: any = {
+        method: "put",
+        headers: { "Authorization": user.token },
+        withCredentials: true
+    }
+    return await Axios.put(`${api_url}/userinfo`, body, config)
+        .then((res) => { console.log(res); return res; })
         .then(handleResponse)
-        .then(response => response.json())
+        .then(response => response.data)
         .then(user => {
             return user;
-    });
+        });
 
 }
 
@@ -60,7 +63,7 @@ async function handleResponse(response: any) {
 
     if (!response.ok) {
         if (response.status === 401) {
-           alert('Token invalid!');
+            alert('Token invalid!');
         }
         const error = response && (response.message || response.statusText);
         return Promise.reject(error);
